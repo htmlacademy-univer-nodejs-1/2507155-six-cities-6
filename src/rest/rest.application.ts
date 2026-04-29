40eb4ed4d22595299e258d60b339b0451768fc57
Kama-Pushka <ser123.ry@gmail.com>
2026-04-29 16:18:04 +0500

    Пример из демо-проекта 7

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,17 +1,25 @@
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import { Controller, ExceptionFilter } from '../shared/libs/rest/index.js';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-  ) {}
+    @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
+    @inject(Component.UserController) private readonly userController: Controller,
+  ) {
+    this.server = express();
+  }
 
   private async initDb() {
     const mongoUri = getMongoURI(
@@ -25,14 +33,44 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+    private async initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
+  private async initControllers() {
+    this.server.use('/users', this.userController.router);
+  }
+
+  private async initMiddleware() {
+    this.server.use(express.json());
+  }
+
+  private async initExceptionFilters() {
+    this.server.use(this.appExceptionFilter.catch.bind(this.appExceptionFilter));
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
-    this.logger.info(`Get value from env $SALT: ${this.config.get('SALT')}`);
-    this.logger.info(`Get value from env $DB_HOST: ${this.config.get('DB_HOST')}`);
 
     this.logger.info('Init database...');
     await this.initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init app-level middleware');
+    await this.initMiddleware();
+    this.logger.info('App-level middleware initialization completed');
+
+    this.logger.info('Init controllers');
+    await this.initControllers();
+    this.logger.info('Controller initialization completed');
+
+    this.logger.info('Init exception filters');
+    await this.initExceptionFilters();
+    this.logger.info('Exception filters initialization compleated');
+
+    this.logger.info('Try to init server...');
+    await this.initServer();
+    this.logger.info(`🚀 Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
