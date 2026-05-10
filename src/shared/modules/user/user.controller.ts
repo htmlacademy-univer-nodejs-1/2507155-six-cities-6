0f282dad8f9c033d9a49fcf78a0f6041ff4085af
Kama-Pushka <ser123.ry@gmail.com>
2026-05-10 14:28:06 +0500

    типы юзера в папку types

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -4,12 +4,12 @@ import { StatusCodes } from 'http-status-codes';
 import { BaseController, HttpError, HttpMethod } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { Component } from '../../types/index.js';
-import { CreateUserRequest } from './create-user-request.type.js';
+import { CreateUserRequest } from './types/create-user-request.type.js';
 import { UserService } from './user-service.interface.js';
 import { Config, RestSchema } from '../../libs/config/index.js';
 import { fillDTO } from '../../helpers/index.js';
 import { UserRdo } from './rdo/user.rdo.js';
-import { LoginUserRequest } from './login-user-request.type.js';
+import { LoginUserRequest } from './types/login-user-request.type.js';
 
 @injectable()
 export class UserController extends BaseController {
